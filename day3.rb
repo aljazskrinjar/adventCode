8f3f18911301caa2dafaea2e31b46602aa946292
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-12-11 12:34:12 +0100

    day3 reafactoring

--- day3.rb
+++ day3.rb
@@ -2,40 +2,14 @@ x = Math.sqrt(ARGV[0].to_f).ceil
 
 unless x.odd? == true then x=x+1 end
 
-numCount = (x-1)*4
+  total = x**2
+  num= ARGV[0].to_i
+  numbers = []
 
-puts x,numCount
-total = x**2
-corner1= total
-
-puts total
-total = total-(x-1)
-corner2=total
-
-puts total
-total = total-(x-1)
-corner3=total
-
-puts total
-total = total-(x-1)
-corner4=total
-
-puts total
-total = total-(x-1)
-corner5=total
-
-num= ARGV[0].to_i
-
-
-
-numbers = []
-
-numbers << (num - corner1).abs
-numbers << (num - corner2).abs
-numbers << (num - corner3).abs
-numbers << (num - corner4).abs
-numbers << (num - corner5).abs
+    5.times {
+      numbers << (num - total).abs
+      total = total-(x-1)
+    }
 
 minimal = numbers.min
-
-puts (x - (minimal +1))
+puts "Number of steps: " + (x - (minimal +1)).to_s
